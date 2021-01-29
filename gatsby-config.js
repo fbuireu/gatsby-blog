52f6a2cca4c6455c192b1131428bdbd5ae737b34
nikkipeel <npeel3497@gmail.com>
2021-01-29 10:24:33 -0500

    fix icon path in config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `${__dirname}/images/blog-logo.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/public/images/blog-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-feed`,

a4d320b884429c94a039e5c6256a30405b393901
nikkipeel <npeel3497@gmail.com>
2021-01-30 13:07:22 -0500

    fix gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,6 +17,8 @@ exports.createPages = ({actions, graphql}) => {
                         title
                         date
                         author
+                        authorImage
+                        image
                         category
                         description
                     }
